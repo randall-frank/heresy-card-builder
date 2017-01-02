e52a4491aab91844db45376e2744783a8b121ad9
frogboots <randall-frank@computer.org>
2017-01-02 15:35:51 -0500

    about and resources working.

--- card_editor.py
+++ card_editor.py
@@ -8,8 +8,10 @@ import argparse
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-import card_editor_res
+import card_editor_res_rc
 from ui_card_editor_main import Ui_card_editor_main
+
+__version__ = "0.1.0.0"
 
 
 class CardEditorMain(QtWidgets.QMainWindow, Ui_card_editor_main):
@@ -17,8 +19,12 @@ class CardEditorMain(QtWidgets.QMainWindow, Ui_card_editor_main):
         super(CardEditorMain, self).__init__(parent)
         self.setupUi(self)
 
-
-__version__ = "0.1.0.0"
+    @QtCore.pyqtSlot()
+    def do_about(self):
+        s = "T.I.M.E Stories card editor\n"
+        s += "Copyright (C) 2017 Randall Frank\n"
+        s += "Version: " + __version__
+        QtWidgets.QMessageBox.about(self, "Card Editor", s)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Edit/process T.I.M.E Stories cards from art assets.')
@@ -31,5 +37,5 @@ if __name__ == '__main__':
     main_win = CardEditorMain()
     main_win.show()
     if args.cardfile:
-        print("Reading {}".format(args.cardfile))
+        print("Reading {}\n".format(args.cardfile))
     sys.exit(app.exec_())
