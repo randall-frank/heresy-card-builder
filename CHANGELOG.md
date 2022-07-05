f582d335f75533e0efcf83a409429cef2f40c4af
Unknown <randall-frank@computer.org>
2022-07-04 22:26:57 -0400

    Add changelog info

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [0.9.0] - WIP
+### Changed
+- Transfer project to github and begin setup.py migration.
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from distutils.core import setup
 
 REQUIREMENTS = [line.strip() for line in open("requirements.txt").readlines()]
 
-curr_dir = os.path.abspath(os.path.dirname(__file__))
+root = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_file_text(file_name):
-    with open(os.path.join(curr_dir, file_name), "r", encoding="utf-8") as in_file:
+    with open(os.path.join(root, file_name), "r", encoding="utf-8") as in_file:
         return in_file.read()
 
 
 version = {}
-_version_file = os.path.join(curr_dir, "_version.py")
+_version_file = os.path.join(root, "_version.py")
 with open(_version_file) as fp:
     exec(fp.read(), version)
 
@@ -25,11 +25,11 @@ setup(name='heresy-card-builder',
       author_email='frogboots.000@gmail.com',
       url='https://github.com/randall-frank/heresy-card-builder',
       project_urls={'Information': 'http://heresy.mrtrashcan.com/'},
-      packages=['distutils', 'distutils.command'],
+      packages=['heresy-card-builder'],
       python_requires=">=3.8",
       license="MIT",
       package_dir={"": "src"},
-      long_description=get_file_text("README.md"),
+      long_description=get_file_text("README.md") + "\n\n" + get_file_text("CHANGELOG.md"),
       long_description_content_type="text/markdown",
       install_requires=REQUIREMENTS,
       )
