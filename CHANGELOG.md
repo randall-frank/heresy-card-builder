a778660a5d189ce43cb8b45fa8c41a20b7e7b6dc
Randall Frank <frogboots.000@gmail.com>
2025-01-15 15:29:05 -0500

    Add support for github decks
    Decks stored in github repos can now be rendered directly.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
-## [0.9.0]
+## [0.9.2]
 ### Changed
-- Transfer project to github and begin setup.py migration.
+- Direct support for rendering decks in git repos
+- Added GUI option to git clone github projects with "heresycarddeck" topic set
 
 ## [0.9.1]
 ### Changed
@@ -12,3 +13,7 @@ All notable changes to this project will be documented in this file.
 - Updated build system to generate wheel packages
 - Rewrote the docs in .rst
 - Fixed various rendering issues in card generation
+
+## [0.9.0]
+### Changed
+- Transfer project to github and begin setup.py migration.
--- src/heresycardbuilder/build_deck.py
+++ src/heresycardbuilder/build_deck.py
@@ -5,12 +5,15 @@
 #
 
 import argparse
+import glob
+import io
 import logging
 import os.path
 import shutil
 import sys
 
 from PySide6 import QtCore, QtWidgets
+from dulwich import porcelain
 
 import heresycardbuilder
 
@@ -20,9 +23,7 @@ from build_pdf import generate_pdf  # noqa: E402
 from build_tts import generate_tts  # noqa: E402
 import card_objects  # noqa: E402
 from card_render import Renderer  # noqa: E402
-from utilities import qt_message_handler  # noqa: E402
-
-# Bug: text word wrapping not correct yet
+from utilities import is_directory, qt_message_handler  # noqa: E402
 
 
 def run() -> None:
@@ -33,8 +34,18 @@ def run() -> None:
         action="version",
         version="%(prog)s {version}".format(version=__version__),
     )
-    parser.add_argument("cardfile", nargs=1, help="The name of a saved project.")
-    parser.add_argument("--outdir", default=None, nargs="?", help="The name of a saved project.")
+    parser.add_argument(
+        "cardfile",
+        nargs=1,
+        help="Filename of a source .deck file or github repo containing a .deck file.",
+    )
+    parser.add_argument(
+        "--outdir",
+        default=None,
+        nargs="?",
+        help="Directory where the 'generated_cards' directory will be created. \
+        By default, it is the directory containing the cardfile.",
+    )
     parser.add_argument(
         "--pad_width", default=0, nargs="?", help="Extra border padding for printing."
     )
@@ -79,14 +90,41 @@ def run() -> None:
     QtCore.qInstallMessageHandler(qt_message_handler)
     app = QtWidgets.QApplication(sys.argv)  # noqa F841
 
+    cardfile = args.cardfile[0]
     if args.default_deck is not None:
-        logging.info("Building deck {}...".format(args.cardfile[0]))
+        logging.info(f"Building deck {cardfile}...")
         deck = card_objects.build_empty_deck(media_dirs=args.default_deck)
         deck.save(args.cardfile[0])
         sys.exit(0)
 
-    logging.info("Reading {}...".format(args.cardfile[0]))
-    filename = os.path.abspath(args.cardfile[0])
+    # if cardfile is a git URL, clone repo into a tempdir and use .deck file in tempdir root.
+    if cardfile.startswith("http") and cardfile.endswith(".git"):
+        if args.outdir is None:
+            logging.error("--outdir must be specified for git source decks")
+            sys.exit(1)
+        destination = args.outdir
+        if not is_directory(destination):
+            logging.error("--outdir must be an empty directory")
+            sys.exit(1)
+        # do the download...
+        logging.info(f"Cloning the git repo: {cardfile} to {destination}")
+        buffer = io.BytesIO()
+        try:
+            porcelain.clone(cardfile, destination, errstream=buffer)
+        except Exception as e:
+            logging.error(f"Unable to clone source git repo: {str(e)}")
+            sys.exit(1)
+        for line in buffer.getvalue().decode("ascii").split("\n"):
+            logging.debug(line)
+        # Look for the .deck file
+        deckfiles = glob.glob(os.path.join(destination, "*.deck"))
+        if len(deckfiles) < 1:
+            logging.error(f"Unable to find .deck file in cloned git repo: {destination}")
+            sys.exit(1)
+        cardfile = deckfiles[0]
+
+    logging.info(f"Reading {cardfile}...")
+    filename = os.path.abspath(cardfile)
     directory = os.path.dirname(filename)
     os.chdir(directory)
     outdir = directory
